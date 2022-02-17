8dfa54429600b565ca6a56a14f1d168e5978f003
Miles Loveday <milesloveday@economist.com>
2022-02-17 09:23:40 +0000

    Basic server

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import * as path from 'path';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/hello', (req, res) => {
+  res.json({message: 'hey you'});
+});
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
