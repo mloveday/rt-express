be6f092fcd387baca3ab250328b07c5e925608a7
Miles Loveday <milesloveday@economist.com>
2022-02-17 17:02:13 +0000

    Create SSE connection & transmit dummy data back & forth

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,3 +1,51 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 
-export const App: React.FC = () => <h1>Hello world!</h1>
+type State = {
+  status: 'empty' | 'done';
+  data: number | undefined;
+}
+
+type Action = {
+  type: 'init' | 'update';
+  payload: number;
+}
+
+const reducer = (state: State, action: Action): State => {
+  console.log(`handling action`, action);
+  switch (action.type) {
+    case 'init':
+    case 'update':
+      return {
+        status: 'done',
+        data: action.payload,
+      }
+    default:
+      return state;
+  }
+}
+
+const initialState: State = { status: 'empty', data: undefined };
+
+export const App: React.FC = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const sse = new EventSource('/api/rt');
+    sse.onmessage = e => {
+      const data = JSON.parse(e.data);
+      console.log(`received message`, data);
+      dispatch({ type: data.type, payload: data.message, })
+    }
+    return () => {
+      console.log(`closing connection`);
+      sse.close();
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <p>Counter value is currently {state.data ?? '[not initialised]'}</p>
+    </div>
+  );
+}
--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,30 @@ app.get('/', (req, res) => {
 });
 
 app.get('/assets/app.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/web.js'));
+  res.sendFile(path.join(__dirname, '../dist/app.js'));
 });
 
-app.get('/api/hello', (req, res) => {
-  res.json({message: 'hey you'});
+app.get('/api/rt', (req, res) => {
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Connection', 'keep-alive');
+  res.flushHeaders(); // flush the headers to establish SSE with client
+
+  const INITIAL_VALUE = 10;
+  res.write(`data: ${JSON.stringify({ type: 'init', message: INITIAL_VALUE })}\n\n`);
+
+  let counter = INITIAL_VALUE;
+  const interval = setInterval(() => {
+    counter++;
+    res.write(`data: ${JSON.stringify({ type: 'update', message: counter })}\n\n`);
+  }, 2000);
+
+  res.on('close', () => {
+    console.log('client disconnected');
+    clearInterval(interval);
+    res.end();
+  });
 });
 
 app.listen(3000, () => {
